5eac3eba0084f3de6470c6d339d785d23c2afbe3
arpit <72805524+arpit164@users.noreply.github.com>
2025-01-27 18:00:32 +0530

    Added Header & Footer

---
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,11 @@
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className="footer">
+            <span>All rights reserved 2025 by asworld</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
---
+++ ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,13 @@
+const HeaderComponent = () => {
+  return (
+    <div>
+      <header>
+        <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+            <a className="navbar-brand" href="#">Employee Management System</a>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
